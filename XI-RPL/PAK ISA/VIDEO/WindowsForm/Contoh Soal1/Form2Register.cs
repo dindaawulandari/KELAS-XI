9fafb1d4fa07cdfe36f0d03b12e014d9fb15ad0f
Dinda Dwi Wulandari <dindgp@gmail.com>
2026-08-11 04:22:17 -0700

    Add files via upload

---
+++ XI-RPL/PAK ISA/VIDEO/WindowsForm/Contoh Soal1/Form2Register.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contoh_Soal1
+{
+    public partial class Form2Register : Form
+    {
+        public Form2Register()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
