aecb0de657567ba71b6cb4299a3742cf6aea69dc
Dinda Dwi Wulandari <dindgp@gmail.com>
2026-08-19 05:33:11 -0700

    Add files via upload

---
+++ XI-RPL/PAK ISA/KELAS/12-AGUSTUS-2026/StopwatchSederhana1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace StopwatchSederhana1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnMulai = new System.Windows.Forms.Button();
+            this.btnBerhenti = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblWaktu = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Stopwatch Sederhana";
+            // 
+            // btnMulai
+            // 
+            this.btnMulai.Location = new System.Drawing.Point(43, 253);
+            this.btnMulai.Name = "btnMulai";
+            this.btnMulai.Size = new System.Drawing.Size(75, 23);
+            this.btnMulai.TabIndex = 1;
+            this.btnMulai.Text = "Mulai";
+            this.btnMulai.UseVisualStyleBackColor = true;
+            this.btnMulai.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnBerhenti
+            // 
+            this.btnBerhenti.Location = new System.Drawing.Point(194, 253);
+            this.btnBerhenti.Name = "btnBerhenti";
+            this.btnBerhenti.Size = new System.Drawing.Size(75, 23);
+            this.btnBerhenti.TabIndex = 1;
+            this.btnBerhenti.Text = "Berhenti";
+            this.btnBerhenti.UseVisualStyleBackColor = true;
+            this.btnBerhenti.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(355, 253);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(210, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Waktu";
+            // 
+            // lblWaktu
+            // 
+            this.lblWaktu.AutoSize = true;
+            this.lblWaktu.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWaktu.Location = new System.Drawing.Point(161, 129);
+            this.lblWaktu.Name = "lblWaktu";
+            this.lblWaktu.Size = new System.Drawing.Size(157, 40);
+            this.lblWaktu.TabIndex = 2;
+            this.lblWaktu.Text = "00 : 00 : 00";
+            this.lblWaktu.Click += new System.EventHandler(this.lblWaktu_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 314);
+            this.Controls.Add(this.lblWaktu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnBerhenti);
+            this.Controls.Add(this.btnMulai);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Stopwatch Sederhana";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMulai;
+        private System.Windows.Forms.Button btnBerhenti;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblWaktu;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ XI-RPL/PAK ISA/KELAS/12-AGUSTUS-2026/StopwatchSederhana1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopwatchSederhana1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int detik = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            detik++;
+
+            int jam = detik / 3600;
+            int menit = detik / 60;
+            int detikSisa = detik % 60;
+
+            lblWaktu.Text = $"{jam:00}:{menit:00}:{detikSisa:00}";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            detik = 0;
+            lblWaktu.Text = "00 : 00";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void lblWaktu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
